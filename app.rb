39399f7350d700eda151aac6c23ed30727f07695
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2012-09-04 22:39:36 +0400

    Add some lectures

--- app.rb
+++ app.rb
@@ -9,12 +9,22 @@ end
 get '/pages/:name' do
   name = params[:name]
   text = File.read "#{settings.root}/pages/#{name}.md"
-  text = MarkdownParser.new(text.gsub(?`, ?$)).to_html
-  @page = text.gsub ?$, ?`
+  text = MarkdownParser.new(prepare(text)).to_html
+  @page = unprepare text
   puts @page
   slim :page
 end
 
 get '/style.css' do
   scss :style
+end
+
+private
+
+def prepare text
+  text.gsub(?`, '$esc1$').gsub(?_, '$esc2$')
+end
+
+def unprepare text
+  text.gsub('$esc1$', ?`).gsub('$esc2$', ?_)
 end
